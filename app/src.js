f78c4933419f9b39290eac1e75eb0d2f59d5ed48
pobertraterson <pobraterson@icloud.com>
2026-06-09 15:48:19 +0100

    Got it working so far

--- app/src.js
+++ app/src.js
@@ -1,1 +1,30 @@
-const htmlToImage = require('html-to-image');
+const videoTitle = document.getElementById('videoTitle');
+const ageRatingOut = document.getElementById('ageRatingOut');
+
+const input = document.querySelector('#videoTitleInput');
+const ageRating = document.querySelectorAll('input[name="ageRating"]');
+
+document.addEventListener('DOMContentLoaded', event => {
+})
+
+input.addEventListener('input', (event) => {
+    videoTitle.textContent = event.target.value;
+});
+
+ageRating.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+      let currentValue = event.target.value;
+      console.log(currentValue);
+      if (currentValue === '18') {
+          ageRatingOut.style.color = 'red';
+          currentValue = currentValue + "+";
+      } else if (currentValue === '15') {
+          ageRatingOut.style.color = 'yellow';
+          currentValue = currentValue + "+";
+      } else {
+          ageRatingOut.style.color = 'white';
+          currentValue = currentValue + "+";
+      }
+      ageRatingOut.textContent = currentValue;
+  })
+})
